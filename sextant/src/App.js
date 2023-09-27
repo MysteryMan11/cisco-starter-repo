14398d8e2df268d9b562883a31aae48828d426b4
MysteryMan1112 <66222060+MysteryMan11@users.noreply.github.com>
2023-09-27 21:19:22 +0000

    task_2
    Signed-off-by: MysteryMan1112 <66222060+MysteryMan11@users.noreply.github.com>

--- sextant/src/App.js
+++ sextant/src/App.js
@@ -3,16 +3,16 @@ import './App.css';
 import React from 'react';
 import Banner from './components/Banner';
 import Exhibit from './components/Exhibit';
-
+import IPAddress from './components/IPaddress';
 
 function App() {
+
   return (
-    <div className="App">
-        <Banner />
-        <Exhibit >
-            <p>Sample Content</p>
-        </Exhibit>
-    </div>
+      <div className="App">
+          <Banner/>
+          <Exhibit/>
+          <IPAddress/>
+      </div>
   );
 }
 
